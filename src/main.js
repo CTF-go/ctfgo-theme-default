bda5b8f549455cd9b120264b61239c022015cec5
Am473ur <1175078221@qq.com>
2021-04-20 17:40:38 +0800

    Rebuild

--- src/main.js
+++ src/main.js
@@ -1,15 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
 
-import './styles/global.css'
-import axios from 'axios'
-
-// axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
-
-axios.defaults.baseURL = 'http://localhost:8080/v1/'
-Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../views/login.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Login',
-    component: Login
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
