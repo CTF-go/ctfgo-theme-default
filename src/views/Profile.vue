d64f56331aab4a5d7ae4a59eff2676756ef7bcc7
Am473ur <1175078221@qq.com>
2021-05-07 16:00:43 +0800

    Add Profile Page

---
+++ src/views/Profile.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Navbar/>
+        <h1>hello world</h1>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+export default {
+    name: 'Profile',
+    components: { Navbar, Footer }
+}
+</script>
