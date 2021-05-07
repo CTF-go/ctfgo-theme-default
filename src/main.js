51f9927d7c96027951a87bdee1bd2a109e67d453
Am473ur <1175078221@qq.com>
2021-05-07 16:07:51 +0800

    Add vue-fontawesome

--- src/main.js
+++ src/main.js
@@ -4,7 +4,13 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import './plugins/vuesax.js'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 axios.defaults.baseURL = 'http://localhost:8081/v1/'
 Vue.prototype.$http = axios
