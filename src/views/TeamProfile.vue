861fdffef87afff303e4a885dfe8b44c01b62859
Am473ur <1175078221@qq.com>
2021-11-21 17:10:45 +0800

    setup team profile page

---
+++ src/views/TeamProfile.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="profile">
+        <h1>Profile</h1>
+        <a>username: </a><br>
+        <a>username: </a><br>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Profile',
+    data:() => ({
+        username: 'admin',
+    }),
+}
+</script>
+<style scoped>
+.profile{
+    margin-top: 84px;
+}
+h1 {
+  color: #24292e;
+  font-size: 5rem;
+  font-weight: normal;
+  line-height: 48px;
+  margin: 0 0 50px;
+  text-align: center;
+  text-shadow: 1px 1px 2px #082b34;
+  user-select: none;
+}
+</style>
