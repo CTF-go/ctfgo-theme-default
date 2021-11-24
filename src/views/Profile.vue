3a6633fa974fed3ac8a6c8b725d06bbe35f303b3
Am473ur <1175078221@qq.com>
2021-11-25 01:32:39 +0800

    完善个人信息页面

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,32 +1,58 @@
 <template>
-    <div class="profile">
-        <h1>Profile</h1>
-        <a>token: {{token}}</a><br>
-        <a>country: {{country}}</a><br>
-        <a>score: {{score}}</a><br>
-        <a>username: {{username}}</a><br>
-        <a>email: {{email}}</a><br>
+  <div class="profile">
+    <h1>{{ username }}</h1>
+    <h2>{{ score }} points</h2>
 
-    </div>
+
+      <vs-button style="display:block;margin:0 auto" @click="copyToken" flat success animation-type="vertical">
+          Token: {{ token }} <template #animate> Copy </template>
+      </vs-button>
+      <br>
+      <vs-button style="display:block;margin:0 auto" flat success animation-type="vertical">
+          Email: {{ email }}
+      </vs-button>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Profile',
-    data:() => ({
-        token: '24be9ccff4467de283b9c58fb3127970',
-        country: "cn",
-        id: 2,
-        score: 100,
-        username: 'admin',
-        email: 'admin@qq.com',
-        score: 100,
-    }),
-}
+  name: "Profile",
+  data: () => ({
+    token: "24be9ccff4467de283b9c58fb3127970",
+    id: 2,
+    score: 100,
+    username: "admin",
+    email: "admin@qq.com",
+    score: 100,
+  }),
+  methods: {
+    copyToken() {
+      console.log("click")
+      const input = document.createElement("input");
+      document.body.appendChild(input);
+      input.setAttribute("value", this.token);
+      input.select();
+      if (document.execCommand("copy")) {
+        document.execCommand("copy");
+      }
+      document.body.removeChild(input);
+    },
+  },
+};
 </script>
+
 <style scoped>
-.profile{
-    margin-top: 84px;
+.profile {
+  margin-top: 84px;
+  text-align: center;
+}
+.button-token{
+  position: absolute;
+  left: 50%;
+}
+.button-email{
+  position: absolute;
+  left: 50%;
 }
 h1 {
   color: #24292e;
